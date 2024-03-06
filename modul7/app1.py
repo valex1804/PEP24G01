164927ef8c68fd620ea9f059a4ffe42f63bbc6d0
Valex <vita.alexandru@outlook.com>
2024-03-06 20:58:45 +0200

    #

--- modul7/app1.py
+++ modul7/app1.py
@@ -1,12 +1,18 @@
+from modul7.categorii import Haine, Accesorii, Incaltaminte
+
+
 class Shop:
     main_menu_options = {1: 'Categorii', 2: 'Produse', 3: 'Iesire'}
-    stoc = {}
+    products_menu_options = {1: 'Adaugare', 2: 'Vizualizare', 3: 'Iesire la meniul principal'}
+    user_select_message = f'Alege optiune:'
+    product_select_message = f'Introduceti optiunea:'
+    product_mapping = {'Haine': Haine, 'Accesorii': Accesorii, 'Incaltaminte': Incaltaminte}
+    stoc = []
     user_input = None
     status = None
 
     def print_main_menu(self):
         menu = 'Bun venit la magazinul Pycharm: \n'
-        user_select_message = f'Alege optiune:'
         options = '\n'.join([f'\t{key}. {value}' for key, value in self.main_menu_options.items()])
         print(menu + options + '\n')
 
@@ -20,18 +26,35 @@ class Shop:
                 pass
             print('Nu este o valoare corecta pentru meniu')
         return option
-            #else:
-                #print('Nu este o valoare corecta pentru meniu')
 
+    def print_categories(self):
+        print("=" * 40)
+        print(" CATEGORII ".center(40, "="))
+        print("=" * 40)
+        print("\n".join(set([f"---\t{cat.__class__.__name__}" for cat in self.stoc])))
 
-    def adauga_prod(self):
-        produs, pret, stoc = input('give product, price, stoc: ').split(',')
-        pret = int(pret)
-        stoc = int(stoc)
-        self.stoc.update({produs: (pret, stoc)})
+    def print_products(self):
+        print(" PRODUSE ".center(40, "="))
+        print("=" * 40)
+        options = '\n'.join([f'\t{key}. {value}' for key, value in self.products_menu_options.items()])
+        print(options + '\n')
+
+    def add_product(self):
+        categorie = input('Introduceti numele categoriei:')
+        produs = input('Introduceti numele produsului:')
+        pret = input('Introduceti pret:')
+        stoc = input('Introduceti stoc:')
+        prod_class = self.product_mapping[categorie.capitalize()]
+        product = prod_class(produs, pret, stoc)
+        self.stoc.append(product)
 
     def show_stock(self):
-        print(self.stoc)
+        for obj in self.stoc:  # type: Haine
+            print(obj.__class__.__name__)
+            print('=' * 30)
+            print(obj.nume)
+            print(obj.pret)
+            print(obj.stoc)
 
     def remove_prod(self):
         product, price, qty = input('give product, price, quantity: ').split(',')
@@ -47,22 +70,24 @@ class Shop:
         self.stoc.update({product: (new_price, self.stoc[product][1])})
 
     def start(self):
-        print(self.menu)
-        self.user_input = input(self.user_select_message)
-        while self.user_input != '5':
-            if self.user_input == '1':
+        self.print_main_menu()
+        option = self.get_user_option(self.user_select_message, self.main_menu_options)
+        if option == 1:
+            self.print_categories()
+        elif option == 2:
+            self.print_products()
+            option2 = self.get_user_option(self.product_select_message, self.products_menu_options)
+            if option2 == 1:
+                self.add_product()
+            elif option2 == 2:
                 self.show_stock()
-            elif self.user_input == '2':
-                self.adauga_prod()
-            elif self.user_input == '3':
-                self.remove_prod()
-            elif self.user_input == '4':
-                self.reducere()
-            print(self.menu)
-            self.user_input = input(self.user_select_message)
 
 
 if __name__ == "__main__":
     s = Shop()
-    s.print_main_menu()
-    s.get_user_option(f'Alege optiune:', Shop.main_menu_options)
+    # s.print_main_menu()
+    # s.get_user_option(f'Alege optiune:', Shop.main_menu_options)
+    s.stoc.append(Haine(nume='tricouri', pret=100, stoc=200))
+    s.stoc.append(Haine(nume='pantaloni', pret=120, stoc=250))
+    s.stoc.append(Accesorii(nume='bratara', pret=10, stoc=1000))
+    s.start()
